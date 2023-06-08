a3a3bb71bb37fc8dbb2a7b40601e76402d150051
Adam Tarasov <adamtarasov@gmail.com>
2023-06-08 12:55:32 +0300

    modifying malloc.c and adding pointers_summary.c

--- malloc.c
+++ malloc.c
@@ -15,5 +15,16 @@ int main(void) {
     printf("I stored %d at memory location %p.\n", *a, a);
     free(b);
     free(c);
+
+    //------------------------------------
+    int * array;
+    array = (int *) malloc(5*sizeof(int));
+    array[0] = 3;
+    array[1] = 44;
+    array[2] = 2;
+    * (array + 3) = 7;
+    * (array + 4) = -1;
+    free(array);
+
     return 0;
 }
